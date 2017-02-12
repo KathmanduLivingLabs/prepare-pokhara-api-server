2db42780950f7eaac9fe42afd20ea445f8ad824c
drklrd <srvbhattarai@gmail.com>
2017-02-12 15:38:58 +0545

    add middleware for regional stats

--- controllers/api/v1/stats.js
+++ controllers/api/v1/stats.js
@@ -2,6 +2,17 @@ import config from '../../../config';
 import geoJSONParser from "../../../libs/geojson-parser";
 import statsCalculator from "../../../libs/statscalculator";
 
+function compare(overall,selection,statsobj,stat){
+	for (var metric in overall) {
+		if (!(selection && selection[metric])) {
+			selection[metric] = 0;
+		}
+		var relativeRegion = (selection[metric] / statsobj[metric]) * 100;
+		stat[metric] = relativeRegion > 0.5 ? Math.round(relativeRegion) : Math.round(relativeRegion * 100) / 100;
+	}
+
+}
+
 export default {
 
 	total: (req, res, next) => {
@@ -23,6 +34,17 @@ export default {
 
 	},
 
+	region : (req,res,next)=>{
+
+		req.stats.region = new statsCalculator(req.cdata.geojson.features, req.collects.type, config.statsIndicator[req.collects.type])
+			.calculate('region');
+
+		req.stats.regionInsights = {};
+
+		
+		next();
+	},
+
 	compare: (req, res, next) => {
 
 		req.stats.insights = {};
@@ -30,17 +52,10 @@ export default {
 		req.stats.selection = (req.cdata.geojson.features && req.cdata.geojson.features.length) ? new statsCalculator(req.cdata.geojson.features, req.collects.type, config.statsIndicator[req.collects.type])
 			.calculate('selection') : {}
 
-		for (var metric in req.stats.overall) {
+		var selectionStats = compare(req.stats.overall,req.stats.selection,req.stats.overall,req.stats.insights);
 
-			if (!(req.stats.selection && req.stats.selection[metric])) {
+		// var regionStats = compare(req.stats.overall,req.stats.selection,req.stats.region,req.stats.regionInsights);
 
-				req.stats.selection[metric] = 0;
-			}
-
-			var relative = (req.stats.selection[metric] / req.stats.overall[metric]) * 100;
-			req.stats.insights[metric] = relative > 0.5 ? Math.round(relative) : Math.round(relative * 100) / 100;
-
-		}
 
 		req.cdata.stats = req.stats;
 		funsole.log(req.cdata, 'white', 'blue');
