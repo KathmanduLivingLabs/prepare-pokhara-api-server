2fd897a87223a4ad2188ad96bdd7444622837c55
drklrd <srvbhattarai@gmail.com>
2017-07-12 15:07:43 +0545

    change API DOC url

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/extracts',  express.static(__dirname + '/extracts'));
-app.use(express.static(path.join(__dirname, '/apidocs')));
+app.use('/apidocs', express.static(path.join(__dirname, '/apidocs')));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
